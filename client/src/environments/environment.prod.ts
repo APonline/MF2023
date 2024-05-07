ae5511839469cb7767bdaaddf5d2614504a149fc
apanemia <andrewgphillips74@gmail.com>
2024-05-07 18:02:41 -0400

    fixing socket port

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   // apiUrl: 'http://127.0.0.1:4000/api/v1/',
 	//SOCKET_ENDPOINT: 'http://127.0.0.1:4000',
   apiUrl: 'https://musefactory.app/api/v1/',
-	SOCKET_ENDPOINT: 'https://musefactory.app:3001',
+	SOCKET_ENDPOINT: 'https://musefactory.app:4001',
   version: 'v1.0.0',
   root: ""
 };
